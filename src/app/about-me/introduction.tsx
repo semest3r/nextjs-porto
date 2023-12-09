42567234236bc12740590053f6d44b16a0ddf11d
Semest3r <gamekusus00@gmail.com>
2023-12-09 09:03:25 +0700

    update

--- src/app/about-me/introduction.tsx
+++ src/app/about-me/introduction.tsx
@@ -1,7 +1,7 @@
 export default function Introduction() {
     return (
         <p className="mt-1 text-justify">
-            Hello, I'm Achmad NurFaizy, an undergraduate student specializing in
+            Hello, I&apos;m Achmad NurFaizy, an undergraduate student specializing in
             Information Systems. As a dedicated web developer, I thrive on
             transforming ideas into interactive digital experiences. Beyond the
             digital realm, my passion extends to the world of online gaming,
