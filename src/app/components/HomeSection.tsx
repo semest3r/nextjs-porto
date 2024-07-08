f623dbbc77d4776a89291a0f09d12c5503264784
Semest3r <gamekusus00@gmail.com>
2024-07-09 00:20:56 +0700

    update

--- src/app/components/HomeSection.tsx
+++ src/app/components/HomeSection.tsx
@@ -1,12 +1,34 @@
+'use client';
+
 import { AppDevelopmentImg, PencilImg } from "@/SharedComponents/Images";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function HomeSection() {
+
+
+    let i = 0;
+    const text = "Innovation";
+    const speed = 1000
+    const generateText = () => {
+        if (i < text.length) {
+            document.getElementById("innovation")!.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(generateText, speed);
+        }
+    }
+    useEffect(() => {
+        setInterval(() => {
+            document.getElementById("innovation")!.innerHTML = '';
+            generateText();
+        }, 11000)
+    }, [])
+
     return (
         <section id="home" className="px-16 bg-[#9057EA] min-h-screen max-h-full w-full flex gap-8 items-center">
             <div className="pl-32 pr-16 flex-1 space-y-8">
                 <AppDevelopmentImg className="mx-auto" />
-                <h1 className="text-center text-4xl">From Ideas to Innovation</h1>
+                <h1 className="text-center text-4xl">From Ideas to <span id="innovation"></span></h1>
                 <p className="text-center text-lg">FullStack Dev | Focused on delivering impactful solutions through internal projects.</p>
                 <div className="flex gap-4 justify-center">
                     <Link href="#experience" className="px-6 py-4 bg-[#FFA124] drop-shadow-[8px_8px_0px_rgba(65,65,65,1)] rounded-2xl">Experience</Link>
