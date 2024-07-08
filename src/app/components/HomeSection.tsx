1dee7f153bd82ce1cf41851d7f49e833f2449e5b
Semest3r <gamekusus00@gmail.com>
2024-07-09 00:43:14 +0700

    update pulse

--- src/app/components/HomeSection.tsx
+++ src/app/components/HomeSection.tsx
@@ -30,7 +30,7 @@ export default function HomeSection() {
         <section id="home" className="px-16 bg-[#9057EA] min-h-screen max-h-full w-full flex gap-8 items-center">
             <div className="pl-32 pr-16 flex-1 space-y-8">
                 <AppDevelopmentImg className="mx-auto" />
-                <h1 className="text-center text-4xl">From Ideas to <span id="innovation"></span></h1>
+                <h1 className="text-center text-4xl">From Ideas to <span id="innovation"></span><span className="animate-[pulse_1s_infinite] opacity-100">|</span></h1>
                 <p className="text-center text-lg">FullStack Dev | Focused on delivering impactful solutions through internal projects.</p>
                 <div className="flex gap-4 justify-center">
                     <Link href="#experience" className="px-6 py-4 bg-[#FFA124] drop-shadow-[8px_8px_0px_rgba(65,65,65,1)] rounded-2xl">Experience</Link>
