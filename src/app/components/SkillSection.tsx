bfb2be61bbc578e7c45f7e0cbd447acf499189ba
Semest3r <gamekusus00@gmail.com>
2024-09-03 20:19:29 +0700

    update skill icon

--- src/app/components/SkillSection.tsx
+++ src/app/components/SkillSection.tsx
@@ -1,4 +1,4 @@
-import { JavaScriptIcon, LaravelIcon, MariadbIcon, NextJSIcon, PhpIcon, PostgreIcon, PythonIcon, ReactIcon, VueIcon } from "@/SharedComponents/Icons"
+import { CsharpIcon, DotnetIcon, JavaScriptIcon, LaravelIcon, MariadbIcon, NextJSIcon, PhpIcon, PostgreIcon, PythonIcon, ReactIcon, VueIcon } from "@/SharedComponents/Icons"
 
 export default function SkillSection() {
     return (
@@ -9,6 +9,12 @@ export default function SkillSection() {
             <div className="mt-2 h-[4px] w-[300px] bg-[#9057EA] mx-auto rounded-2xl"></div>
             <div className="px-12 py-8 grid grid-cols-2 sm:grid-cols-7 place-content-center gap-8">
                 <div className="p-4 border-2 self-center border-[#414141] bg-white drop-shadow-[8px_8px_0px_rgba(65,65,65,1)] rounded-2xl">
+                    <CsharpIcon />
+                </div>
+                <div className="p-4 border-2 self-center border-[#414141] bg-white drop-shadow-[8px_8px_0px_rgba(65,65,65,1)] rounded-2xl">
+                    <DotnetIcon />
+                </div>
+                <div className="p-4 border-2 self-center border-[#414141] bg-white drop-shadow-[8px_8px_0px_rgba(65,65,65,1)] rounded-2xl">
                     <PhpIcon />
                 </div>
                 <div className="p-4 border-2 self-center border-[#414141] bg-white drop-shadow-[8px_8px_0px_rgba(65,65,65,1)] rounded-2xl">
@@ -16,9 +22,6 @@ export default function SkillSection() {
                 </div>
                 <div className="p-4 border-2 self-center border-[#414141] bg-white drop-shadow-[8px_8px_0px_rgba(65,65,65,1)] rounded-2xl">
                     <JavaScriptIcon />
-                </div>
-                <div className="p-4 border-2 self-center border-[#414141] bg-white drop-shadow-[8px_8px_0px_rgba(65,65,65,1)] rounded-2xl">
-                    <PythonIcon />
                 </div>
                 <div className="p-4 border-2 self-center border-[#414141] bg-white drop-shadow-[8px_8px_0px_rgba(65,65,65,1)] rounded-2xl">
                     <NextJSIcon />
